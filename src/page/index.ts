aa5c0bce0007ffcbf58c1cda03323347b717b65b
Bodhert <you@example.com>
2019-01-16 15:48:46 -0500

    organizing the code with POM

---
+++ src/page/index.ts
@@ -0,0 +1,2 @@
+export { MenuContentPage } from './menu-content.page';
+export { ProductListPage } from './product-list.page';
--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -1,42 +1,24 @@
-import { $, browser } from 'protractor';
+import { browser } from 'protractor';
+import {
+  MenuContentPage,
+  ProductListPage
+} from '../src/page';
 
 describe('When I try to buy a t-shirt', () => {
+  beforeAll(async () => {
+    await browser.get('http://automationpractice.com/');
+  });
 
   beforeEach(() => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
   });
 
   it('Then a t-shirt should be bought', async () => {
-    await browser.get('http://automationpractice.com/');
-    await(browser.sleep(10000));
-    await $('#block_top_menu > ul > li:nth-child(3) > a').click();
-    await(browser.sleep(3000));
-    await $('#center_column a.button.ajax_add_to_cart_button.btn.btn-default').click();
-    await(browser.sleep(3000));
-    await $('[style*="display: block;"] .button-container > a').click();
-    await(browser.sleep(3000));
-    await $('.cart_navigation span').click();
-    await(browser.sleep(3000));
+    const menuContentPage: MenuContentPage = new MenuContentPage();
+    const productListPage: ProductListPage = new ProductListPage();
 
-    await $('#email').sendKeys('aperdomobo@gmail.com');
-    await $('#passwd').sendKeys('WorkshopProtractor');
-    await $('#SubmitLogin > span').click();
-    await(browser.sleep(3000));
-
-    await $('#center_column > form > p > button > span').click();
-    await(browser.sleep(3000));
-
-    await $('#cgv').click();
-    await(browser.sleep(3000));
-
-    await $('#form > p > button > span').click();
-    await(browser.sleep(3000));
-    await $('#HOOK_PAYMENT > div:nth-child(1) > div > p > a').click();
-    await(browser.sleep(3000));
-    await $('#cart_navigation > button > span').click();
-    await(browser.sleep(3000));
-
-    await expect($('#center_column > div > p > strong').getText())
-      .toBe('Your order on My Store is complete.');
+    await menuContentPage.goToTShirtMenu();
+    await productListPage.addToCart('Faded Short Sleeve T-shirts');
+    console.log('dfsadf');
   });
 });
