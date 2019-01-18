14812c436bcbbef0b192dab745df1b19da685291
Bodhert <you@example.com>
2019-01-18 14:19:49 -0500

    fixing travis

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -1,17 +1,27 @@
 import { Config, browser } from 'protractor';
 import { reporter } from './helpers/reporter';
 
-export const config: Config = {
+export let config: Config = {
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
-  getPageTimeout: 3000,
   SELENIUM_PROMISE_MANAGER: false,
-  onPrepare: () => {
-    browser.manage().timeouts().implicitlyWait(0);
-    browser.ignoreSynchronization = true;
-    reporter();
+  noGlobals: true,
+  getPageTimeout: 30000,
+  capabilities: {
+    browserName: 'chrome',
+    shardTestFiles: true,
+    maxInstances: 2,
+    chromeOptions: {
+      args: ['disable-infobars=true', '--window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
   },
   jasmineNodeOpts: {
     defaultTimeoutInterval: 120000
+  },
+  onPrepare: () => {
+    reporter();
+    browser.ignoreSynchronization = true;
+    browser.manage().timeouts().implicitlyWait(0);
   }
 };
