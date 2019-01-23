c16b13be341a968ecbce3ff6142f4835b13a337b
Bodhert <you@example.com>
2019-01-23 13:09:23 -0500

    WIP

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -18,57 +18,53 @@ describe('Given the shopping page', () => {
   });
 
   describe('When we want to buy a t-shirt', async () => {
-    beforeAll(async () => {
-      const menuContentPage: MenuContentPage = new MenuContentPage();
-      const productListPage: ProductListPage = new ProductListPage();
-      const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
-      const summaryStepPage: SummaryStepPage = new SummaryStepPage();
+    const menuContentPage: MenuContentPage = new MenuContentPage();
+    const productListPage: ProductListPage = new ProductListPage();
+    const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
+    const summaryStepPage: SummaryStepPage = new SummaryStepPage();
 
+    beforeAll(async () => {
       await menuContentPage.goToTShirtMenu();
       await productListPage.addToCart('Faded Short Sleeve T-shirts');
       await productAddedModalPage.proceedToCheckout();
       await summaryStepPage.proceedToCheckout();
-
     });
 
     describe('And login to the application', () => {
-      beforeAll(async () => {
-        const signInStepPage: SignInStepPage = new SignInStepPage();
+      const signInStepPage: SignInStepPage = new SignInStepPage();
 
+      beforeAll(async () => {
         await signInStepPage.login('aperdomobo@gmail.com', 'WorkshopProtractor');
       });
 
       describe('and select the default address of delivery', () => {
+        const addressStepPage: AddressStepPage = new AddressStepPage();
         beforeAll(async () => {
-          const addressStepPage: AddressStepPage = new AddressStepPage();
-
           await addressStepPage.proceedToCheckout();
         });
 
         describe('and proceed the default shiping options', () => {
-          beforeAll(async () => {
-            const shippingStepPage: ShippingStepPage = new ShippingStepPage();
+          const shippingStepPage: ShippingStepPage = new ShippingStepPage();
 
+          beforeAll(async () => {
             await shippingStepPage.acceptTermsAndProceedCheckout();
           });
-          describe('and select to pay by bank wire option', () => {
-            beforeAll(async () => {
-              const paymentStepPage: PaymentStepPage = new PaymentStepPage();
 
+          describe('and select to pay by bank wire option', () => {
+            const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+            beforeAll(async () => {
               await paymentStepPage.payByBankWire();
             });
 
             describe('and pay to the bank', () => {
+              const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
+              const orderResumePage: OrderResumePage = new OrderResumePage();
               beforeAll(async () => {
-                const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
-
                 await bankPaymentPage.confirmOrder();
               });
 
               it('then the shirt should be bougth', () => {
                 beforeAll(async () => {
-                  const orderResumePage: OrderResumePage = new OrderResumePage();
-
                   await expect(orderResumePage.getOrderTitle())
                     .toBe('Your order on My Store is complete.');
                 });
